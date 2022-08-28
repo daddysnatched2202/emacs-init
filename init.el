a8cf7c824eb32d73e7a18939ae55b91f44ddd5fb
Curtis Klassen <daddysnatched2202@gmail.com>
2022-08-28 19:28:38 -0400

    Move lexical binding declaration

--- init.el
+++ init.el
@@ -1,4 +1,5 @@
 ;; -*- lexical-binding: t -*-
+(setq lexical-binding t)
 
 ;; Copyright 2022 Curtis Klassen
 
@@ -13,8 +14,6 @@
 
 ;; You should have received a copy of the GNU General Public License along with this
 ;; program. If not, see <https://www.gnu.org/licenses/>.
-
-(setq lexical-binding t)
 
 ;; straight
 (defvar bootstrap-version)
