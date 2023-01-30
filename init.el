838a44b49e4fd623187bedf7858353a8e60e56b9
Curtis Klassen <daddysnatched2202@gmail.com>
2023-01-29 19:51:53 -0500

    Improve `paredit-space-macros`
    It's annoying for `paredit` to insert a space after `,@`

--- init.el
+++ init.el
@@ -1,5 +1,5 @@
 ;; -*- lexical-binding: t -*-
-;; Copyright 2022 Curtis Klassen
+;; Copyright 2022-2023 Curtis Klassen
 
 ;; This program is free software: you can redistribute it and/or modify it under the
 ;; terms of the GNU General Public License as published by the Free Software
@@ -185,7 +185,8 @@
 (defun paredit-space-macros (endp delimiter)
   (or endp
       (if (eq (char-syntax delimiter) ?\()
-	  (not (looking-back "#."))
+	  (not (or (looking-back "#.")
+                   (looking-back ",.")))
 	t)))
 
 (use-package paredit
