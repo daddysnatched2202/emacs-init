eafcb10acb4b400c2a5e481901ec828a0d046217
Curtis Klassen <daddysnatched2202@gmail.com>
2023-04-18 10:49:35 -0400

    Fix (again) fill column indicators

--- init.el
+++ init.el
@@ -410,6 +410,7 @@ the path down to `max-len'"
 (blink-cursor-mode 1)
 (global-visual-line-mode 1)
 (global-display-line-numbers-mode 1)
+(global-display-fill-column-indicator-mode 1)
 (global-hl-line-mode 1)
 (show-paren-mode 1)
 
@@ -436,7 +437,7 @@ the path down to `max-len'"
 (setq-default fill-column 85)
 (setq-default auto-fill-function nil)
 (setq-default display-fill-column-indicator-character ?|)
-(add-hook 'change-major-mode-hook 'display-fill-column-indicator--turn-on)
+(setq-default display-fill-column-indicator t)
 
 (setq-default org-src-fontify-natively t)
 
